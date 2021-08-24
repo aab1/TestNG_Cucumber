eee47cb110c3ec981bd65edd7af4f24baaf63688
ybakare <ybakare@trellisrx.com>
2021-08-24 17:31:31 -0400

    committing FailedTestsRunner

--- src/test/java/Parallel/FailedTestsRunner.java
+++ src/test/java/Parallel/FailedTestsRunner.java
@@ -1,26 +1,26 @@
 package Parallel;
 
-
 import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
-import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/Parallel"},//feature file must be in same package as the steps(i.e. package name must be same)
+        features = {"@target/rerunfailedtest.txt"},//feature file must be in same package as the steps(i.e. package name must be same)
         glue = {"Parallel"}, //step def and hook files package.
         plugin = {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "timeline:test-output-thread/",
-                "rerun:target/rerunfailedtest.txt"//failed tests will be added to the file at this location
-        //,tags = "@accounts"
-        }
-
+                "rerun:target/rerunfailedtest.txt"
+                //,tags = "@accounts"
+        },
+        monochrome = true
 )
-public class ParallelRun extends AbstractTestNGCucumberTests {
+
+public class FailedTestsRunner extends AbstractTestNGCucumberTests{
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,59 +1,37 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "799ff3a0-5dac-4ba5-a6bb-f4295f0f7fc0",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1629835355982,
-    "group": 17,
-    "content": "",
-    "tags": "",
-    "end": 1629835362976,
-    "className": "passed"
-  },
-  {
-    "id": "f55c7194-6708-4e72-a3ec-31b7026b4322",
-    "feature": "Login page feature",
-    "scenario": "Forgot Password link",
-    "start": 1629835355937,
-    "group": 18,
-    "content": "",
-    "tags": "",
-    "end": 1629835363587,
-    "className": "passed"
-  },
-  {
-    "id": "e0eca11d-d20b-4e0c-94ca-5788d03b6a12",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts page title",
-    "start": 1629835355982,
-    "group": 15,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1629835371990,
-    "className": "passed"
-  },
-  {
-    "id": "179bfb85-b7d2-4fef-a0a0-6756d86a3510",
+    "id": "db8da83d-74c9-43ef-84ba-d8b32822f7d7",
     "feature": "Account Page Feature",
     "scenario": "Accounts section count",
-    "start": 1629835355983,
+    "start": 1629840516602,
     "group": 16,
     "content": "",
     "tags": "@accounts,",
-    "end": 1629835372209,
-    "className": "passed"
+    "end": 1629840522424,
+    "className": "failed"
   },
   {
-    "id": "aa796070-762d-406f-a8d0-ef0f08c55822",
+    "id": "a674493d-1a8e-4ec5-a70e-944a10fb38a9",
     "feature": "Login page feature",
     "scenario": "Login with correct credentials",
-    "start": 1629835355981,
-    "group": 19,
+    "start": 1629840516554,
+    "group": 17,
     "content": "",
     "tags": "",
-    "end": 1629835372332,
-    "className": "passed"
+    "end": 1629840522344,
+    "className": "failed"
+  },
+  {
+    "id": "4ddc69a4-2862-45bc-ae0d-ae21330fa8a8",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1629840516552,
+    "group": 15,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1629840522378,
+    "className": "failed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
@@ -68,14 +46,6 @@ CucumberHTML.timelineGroups.pushArray([
   {
     "id": 17,
     "content": "Thread[TestNG-PoolService-2,5,main]"
-  },
-  {
-    "id": 18,
-    "content": "Thread[TestNG-PoolService-3,5,main]"
-  },
-  {
-    "id": 19,
-    "content": "Thread[TestNG-PoolService-4,5,main]"
   }
 ]);
 });
